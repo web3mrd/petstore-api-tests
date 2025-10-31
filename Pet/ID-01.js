121351dd22a472105dd24e5ad22a987a47d7df2a
web3mrd <dmitroktech@gmail.com>
2025-11-01 00:06:33 +0300

    Test of comparison of request time

--- Pet/ID-01.js
+++ Pet/ID-01.js
@@ -57,29 +57,33 @@ pm.test("JSON schema validation", function () {
     pm.response.to.have.jsonSchema(schemaPet);
 });
 
-pm.test("Checking values in request headers", function(){
-pm.expect(pm.request.headers.get("accept")).to.eql("application/json");
-pm.expect(pm.request.headers.get("Content-Type")).to.eql("application/json");
-pm.expect(pm.request.headers.get("Cache-Control")).to.eql("no-cache");
-pm.expect(pm.request.headers.get("Host")).to.eql("petstore.swagger.io");
-pm.expect(pm.request.headers.get("Connection")).to.eql("keep-alive");
+pm.test("Checking values in request headers", function () {
+    pm.expect(pm.request.headers.get("accept")).to.eql("application/json");
+    pm.expect(pm.request.headers.get("Content-Type")).to.eql("application/json");
+    pm.expect(pm.request.headers.get("Cache-Control")).to.eql("no-cache");
+    pm.expect(pm.request.headers.get("Host")).to.eql("petstore.swagger.io");
+    pm.expect(pm.request.headers.get("Connection")).to.eql("keep-alive");
 });
 
-pm.test("Checking values in response headers", function(){
-pm.expect(pm.response.headers.get("Content-Type")).to.eql("application/json");
-pm.expect(pm.response.headers.get("Connection")).to.eql("keep-alive");
-pm.expect(pm.response.headers.get("Access-Control-Allow-Origin")).to.eql("*");
-pm.expect(pm.response.headers.get("Access-Control-Allow-Methods")).to.eql("GET, POST, DELETE, PUT");
-pm.expect(pm.response.headers.get("Access-Control-Allow-Headers")).to.eql("Content-Type, api_key, Authorization");
-pm.expect(pm.response.headers.get("Server")).to.eql("Jetty(9.2.9.v20150224)");
+pm.test("Checking values in response headers", function () {
+    pm.expect(pm.response.headers.get("Content-Type")).to.eql("application/json");
+    pm.expect(pm.response.headers.get("Connection")).to.eql("keep-alive");
+    pm.expect(pm.response.headers.get("Access-Control-Allow-Origin")).to.eql("*");
+    pm.expect(pm.response.headers.get("Access-Control-Allow-Methods")).to.eql("GET, POST, DELETE, PUT");
+    pm.expect(pm.response.headers.get("Access-Control-Allow-Headers")).to.eql("Content-Type, api_key, Authorization");
+    pm.expect(pm.response.headers.get("Server")).to.eql("Jetty(9.2.9.v20150224)");
 });
 
-pm.test("Status code 200 or 405", function(){
-    pm.expect(pm.response.code).to.be.oneOf([200,405]);
+pm.test("Status code 200 or 405", function () {
+    pm.expect(pm.response.code).to.be.oneOf([200, 405]);
 });
 
-pm.test("Checking the error message", function(){
+pm.test("Checking the error message", function () {
     if (pm.response.code == 405) {
         pm.expect(response.massage).to.eql("Invalid input")
     };
+});
+
+pm.test("Request time check", function () {
+    pm.expect(pm.response.responseTime).to.be.below(1000);
 });
