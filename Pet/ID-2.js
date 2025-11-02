2d4f77b187e710144a0717c9484a343c7b6672dd
web3mrd <dmitroktech@gmail.com>
2025-11-03 01:42:14 +0300

    ID-2 test fix

--- Pet/ID-2.js
+++ Pet/ID-2.js
@@ -1,7 +1,7 @@
 let responseBody = pm.response.json();
 
 pm.test("Checking required fields in the response body", function (){
-let requiredFields = ["id", "name","photoUrls"];
+let requiredFields = ["name","photoUrls"];
 pm.expect(Object.keys(responseBody)).to.eql(requiredFields);
 });
 
