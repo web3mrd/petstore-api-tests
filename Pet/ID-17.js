bc8ca785797a2cf5ed36e15853d19e2ec18dca94
web3mrd <dmitroktech@gmail.com>
2025-11-05 17:57:50 +0300

    Removed console output

--- Pet/ID-17.js
+++ Pet/ID-17.js
@@ -3,4 +3,3 @@ let responseBody = pm.response.json();
 pm.test("Checking for absence of object category", function(){
     pm.expect(responseBody).to.not.have.property("category");
 });
-console.log(responseBody);
--- Pet/ID-24.js
+++ Pet/ID-24.js
@@ -3,6 +3,5 @@ let petName = responseBody.name;
 
 pm.test("Checking the lower case of the value in the name field", function(){
   let partOfName = petName.charAt(0).toUpperCase() + petName.slice(1).toLowerCase();
-  console.log(partOfName);
   pm.expect(petName).to.eql(partOfName);
 });
